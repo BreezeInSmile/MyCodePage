b85f73279c236a39ba8c5df7a8edf69d212d8be6
Martian Ye <mars_9@163.com>
2019-12-04 22:36:31 +0800

    Demo update

--- MyCodePage/Startup.cs
+++ MyCodePage/Startup.cs
@@ -1,21 +1,35 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MyCode.Models;
+using MyCode.Services;
 
 namespace MyCodePage
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        private readonly IConfiguration _configuration;
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddRazorPages();
+
+            services.AddSingleton<IClock, ChinaClock>();
+            services.AddSingleton<IDepartmentService, DepartmentService>();
+            services.AddSingleton<IEmployeeService, EmployeeService>();
+
+            services.Configure<MyCodeOptions>(_configuration.GetSection("MyCode"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,14 +40,17 @@ namespace MyCodePage
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
+            app.UseStaticFiles();//ʹ�þ�̬�ļ�
 
-            app.UseEndpoints(endpoints =>
+            app.UseHttpsRedirection();//��http����ת��Ϊhttps����
+
+            app.UseAuthentication();//������֤
+
+            app.UseRouting();//·���м��
+
+            app.UseEndpoints(endpoints => //�˵��м��
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapRazorPages();
             });
         }
     }
